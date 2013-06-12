631ebf3476b4a22de76f337150097fd02610b51c
h3ckman <h3ckman@gmail.com>
2013-06-12 12:26:15 -0400

    Changed schedule calendar view, now clickable with dots to indicate events that day

--- Conferences/gen/com/speedway/conferences/R.java
+++ Conferences/gen/com/speedway/conferences/R.java
@@ -10,6 +10,16 @@ package com.speedway.conferences;
 public final class R {
     public static final class attr {
     }
+    public static final class color {
+        public static final int black=0x7f040005;
+        public static final int blue=0x7f040002;
+        public static final int gray=0x7f040006;
+        public static final int green=0x7f040001;
+        public static final int lightgray=0x7f040007;
+        public static final int navy=0x7f040004;
+        public static final int selectblue=0x7f040003;
+        public static final int white=0x7f040000;
+    }
     public static final class dimen {
         /**  Default screen margins, per the Android Design guidelines. 
 
@@ -17,42 +27,50 @@ public final class R {
          screen margins) for sw720dp devices (e.g. 10" tablets) in landscape here.
     
          */
-        public static final int activity_horizontal_margin=0x7f040000;
-        public static final int activity_vertical_margin=0x7f040001;
+        public static final int activity_horizontal_margin=0x7f050000;
+        public static final int activity_vertical_margin=0x7f050001;
     }
     public static final class drawable {
-        public static final int ic_launcher=0x7f020000;
-        public static final int ic_menu_add=0x7f020001;
-        public static final int ic_menu_logout=0x7f020002;
-        public static final int ic_menu_search=0x7f020003;
-        public static final int ic_schedule1=0x7f020004;
-        public static final int schedule_icon=0x7f020005;
-        public static final int speedway=0x7f020006;
+        public static final int arrow_left=0x7f020000;
+        public static final int arrow_right=0x7f020001;
+        public static final int event_dot=0x7f020002;
+        public static final int ic_launcher=0x7f020003;
+        public static final int ic_menu_add=0x7f020004;
+        public static final int ic_menu_logout=0x7f020005;
+        public static final int ic_menu_search=0x7f020006;
+        public static final int ic_schedule1=0x7f020007;
+        public static final int schedule_icon=0x7f020008;
+        public static final int speedway=0x7f020009;
     }
     public static final class id {
-        public static final int action_settings=0x7f080014;
-        public static final int add=0x7f080011;
-        public static final int conferences=0x7f080002;
-        public static final int date=0x7f08000e;
-        public static final int end=0x7f080010;
-        public static final int gridview=0x7f080009;
-        public static final int helpBtn=0x7f080008;
-        public static final int info=0x7f08000b;
-        public static final int list=0x7f080000;
-        public static final int location=0x7f08000d;
-        public static final int loginBtn=0x7f080007;
-        public static final int logout=0x7f080015;
-        public static final int password=0x7f080005;
-        public static final int passwordEdit=0x7f080006;
-        public static final int place=0x7f08000c;
+        public static final int action_settings=0x7f090019;
+        public static final int add=0x7f090015;
+        public static final int conferences=0x7f090002;
+        public static final int date=0x7f090012;
+        public static final int date_icon=0x7f090016;
+        public static final int end=0x7f090014;
+        public static final int gridview=0x7f09000d;
+        public static final int header=0x7f090009;
+        public static final int helpBtn=0x7f090008;
+        public static final int info=0x7f09000f;
+        public static final int list=0x7f090000;
+        public static final int location=0x7f090011;
+        public static final int loginBtn=0x7f090007;
+        public static final int logout=0x7f09001a;
+        public static final int next=0x7f09000c;
+        public static final int password=0x7f090005;
+        public static final int passwordEdit=0x7f090006;
+        public static final int place=0x7f090010;
+        public static final int previous=0x7f09000a;
         public static final int realtabcontent=0x01010000;
-        public static final int reference=0x7f080012;
-        public static final int search=0x7f080013;
-        public static final int speedwayImage=0x7f080001;
-        public static final int start=0x7f08000f;
-        public static final int type=0x7f08000a;
-        public static final int user=0x7f080003;
-        public static final int userEdit=0x7f080004;
+        public static final int reference=0x7f090017;
+        public static final int search=0x7f090018;
+        public static final int speedwayImage=0x7f090001;
+        public static final int start=0x7f090013;
+        public static final int title=0x7f09000b;
+        public static final int type=0x7f09000e;
+        public static final int user=0x7f090003;
+        public static final int userEdit=0x7f090004;
     }
     public static final class layout {
         public static final int activity_events=0x7f030000;
@@ -61,25 +79,26 @@ public final class R {
         public static final int activity_single_event=0x7f030003;
         public static final int activity_speakers=0x7f030004;
         public static final int activity_tab=0x7f030005;
-        public static final int event_row=0x7f030006;
+        public static final int calendar_item=0x7f030006;
+        public static final int event_row=0x7f030007;
     }
     public static final class menu {
-        public static final int events=0x7f070000;
-        public static final int login=0x7f070001;
-        public static final int schedule=0x7f070002;
-        public static final int single_event=0x7f070003;
-        public static final int speakers=0x7f070004;
-        public static final int tab=0x7f070005;
+        public static final int events=0x7f080000;
+        public static final int login=0x7f080001;
+        public static final int schedule=0x7f080002;
+        public static final int single_event=0x7f080003;
+        public static final int speakers=0x7f080004;
+        public static final int tab=0x7f080005;
     }
     public static final class string {
-        public static final int action_settings=0x7f050001;
-        public static final int app_name=0x7f050000;
-        public static final int hello_world=0x7f050002;
-        public static final int title_activity_events=0x7f050005;
-        public static final int title_activity_schedule=0x7f050004;
-        public static final int title_activity_single_event=0x7f050006;
-        public static final int title_activity_speakers=0x7f050007;
-        public static final int title_activity_tab=0x7f050003;
+        public static final int action_settings=0x7f060001;
+        public static final int app_name=0x7f060000;
+        public static final int hello_world=0x7f060002;
+        public static final int title_activity_events=0x7f060005;
+        public static final int title_activity_schedule=0x7f060004;
+        public static final int title_activity_single_event=0x7f060006;
+        public static final int title_activity_speakers=0x7f060007;
+        public static final int title_activity_tab=0x7f060003;
     }
     public static final class style {
         /** 
@@ -103,10 +122,10 @@ public final class R {
     
  API 14 theme customizations can go here. 
          */
-        public static final int AppBaseTheme=0x7f060000;
+        public static final int AppBaseTheme=0x7f070000;
         /**  Application theme. 
  All customizations that are NOT specific to a particular API-level can go here. 
          */
-        public static final int AppTheme=0x7f060001;
+        public static final int AppTheme=0x7f070001;
     }
 }
